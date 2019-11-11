2c3de15c858dfcb3d46e4c1d142fdaee08af971d
Randy Collado <randyro91@gmail.com>
2019-11-11 17:34:51 +0100

    Add trace entities

---
+++ src/Traces.Data/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using NodaTime;
+
+namespace Traces.Data.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Instant CreatedUtc { get; set; }
+
+        [Required]
+        public Instant UpdatedUtc { get; set; }
+    }
+}
