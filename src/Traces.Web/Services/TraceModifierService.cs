dd549ab31ab071a9fd143fe08b991ab7211cc350
Randy Collado <randyro91@gmail.com>
2019-11-20 12:24:30 +0100

    Add unit tests for TraceModifierService

--- src/Traces.Web/Services/TraceModifierService.cs
+++ src/Traces.Web/Services/TraceModifierService.cs
@@ -1,7 +1,7 @@
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using NodaTime.Extensions;
 using Optional;
+using Traces.Common.Exceptions;
 using Traces.Common.Utils;
 using Traces.Core.Models;
 using Traces.Core.Services;
