73844077af2246c3fcdd9bf7862f07bef241df1e
Randy Collado <randyro91@gmail.com>
2019-11-25 18:34:52 +0100

    Add tenant separation db migration

---
+++ src/Traces.Data/Migrations/20191125173338_TenantSeparation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Traces.Data.Migrations
+{
+    public partial class TenantSeparation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompletedBy",
+                table: "Trace",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                table: "Trace",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedBy",
+                table: "Trace");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Trace");
+        }
+    }
+}
