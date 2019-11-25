9d714f9d76845b4ce339ae1711954d637220ba38
Randy Collado <randyro91@gmail.com>
2019-11-25 18:14:34 +0100

    Add apaleo claims and security constants

---
+++ src/Traces.Common/Constants/ApaleoClaims.cs
@@ -0,0 +1,9 @@
+namespace Traces.Common.Constants
+{
+    public static class ApaleoClaims
+    {
+        public const string AccountCode = "account_code";
+
+        public const string SubjectId = "sub";
+    }
+}
---
+++ src/Traces.Common/Constants/SecurityConstants.cs
@@ -0,0 +1,7 @@
+namespace Traces.Common.Constants
+{
+    public static class SecurityConstants
+    {
+        public const string AccessToken = "access_token";
+    }
+}
