8df57f7c8b8c49c48cf7edd166cdcfc3bb546e3f
Randy Collado <randyro91@gmail.com>
2019-11-27 17:41:30 +0100

    Remove adding scope for apaleoClientFactory as addHttpClient adds the scope already

--- src/Traces.Web/Startup.cs
+++ src/Traces.Web/Startup.cs
@@ -51,8 +51,6 @@ namespace Traces.Web
                 .AddBootstrapProviders()
                 .AddFontAwesomeIcons();
 
-            services.AddScoped<IApaleoClientFactory, ApaleoClientFactory>();
-
             // Here we have a retry policy only for read-only requests such as GET or HEAD
             // In addition there is a waiting time for the circuit breaker to avoid too many requests per second to the apaleo api
             services.AddHttpClient<IApaleoClientFactory, ApaleoClientFactory>(client =>
