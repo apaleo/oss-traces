b7eb85aab445e3991838cf54ae2166807c93998f
Randy Collado <randyro91@gmail.com>
2019-11-28 18:14:02 +0100

    Add use authentication

--- src/Traces.Web/Startup.cs
+++ src/Traces.Web/Startup.cs
@@ -139,6 +139,7 @@ namespace Traces.Web
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseForwardedHeaders();
             }
 
             app.UseHttpsRedirection();
@@ -151,6 +152,8 @@ namespace Traces.Web
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+
+            app.UseAuthentication();
 
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
