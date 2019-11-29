7b641a82c5a668f190d93c3ad5590205057359b1
Peter Grman <peter.grman@gmail.com>
2019-11-29 16:56:03 +0100

    coding in github is hard

--- src/Traces.Web/Startup.cs
+++ src/Traces.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
