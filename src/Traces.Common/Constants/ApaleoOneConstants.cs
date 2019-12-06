fac29094b1b874042f569101e3e3275c21f347d6
Randy Collado <randyro91@gmail.com>
2019-12-06 15:41:40 +0100

    Create ui integrations on first connect (#26)
    * Add Integration api client
    
    * Add integration api client factory
    
    * Add missing function to client factory
    
    * Move requestContext initialization from base viewmodel to cookie refresh validation event
    
    * Revert base viewmodel changes
    
    * Parameter mixup
    
    * Move integration client factory to separate factory
    
    * Initial iteration of apaleo setup service
    
    * Add integration config and update appsettings for custom integration parameters
    
    * await for all tasks
    
    * Move request context and modify initialization
    
    * This was for testing purposes
    
    * Update integration client with latest changes
    
    * typo
    
    * Properly perform creation of ui integration and use when refreshing token
    
    * Move traces to a separate page and use index to setup ui integrations
    
    * Handle unsuccessful requests to integration with BusinessValidationExceptions
    
    * Validate integration url
    
    * Revert changes to auto refresh cookie events code
    
    * Missed usings
    
    * Update request context initialization
    
    * Set variable
    
    * Add missing clicked event
    
    * Changes based on comments
    
    * Comment
    
    * Set default integration code and modify creation process
    
    * Modify integration url creation process
    
    * Missed initialization
    
    * Rename TracesPage to TracesAccountPage
    
    * Retry creation if fails
    
    * Revert some changes and make function not return bool
    
    * Check if integration exists even after unsuccessful status code
    
    * Remove return type for initialize context
    
    * Handle integration Url differently

---
+++ src/Traces.Common/Constants/ApaleoOneConstants.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Traces.Common.Constants
+{
+    public static class ApaleoOneConstants
+    {
+        public const string AccountLevelRelativeUrl = "/traces/account";
+
+        public const string ReservationLevelRelativeUrl = "/traces/reservation";
+
+        public const string PropertyLevelRelativeUrl = "/traces/property";
+
+        public const string IntegrationSourceType = "Public";
+    }
+}
