6bcc49e4445f10d85e956b2704e50baee1c0d5b5
Randy Collado <randyro91@gmail.com>
2019-12-06 15:18:22 +0100

    Create db migration

---
+++ src/Traces.Data/Migrations/20191206141717_AddPropertyAndReservationIdFilters.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Traces.Data.Migrations
+{
+    public partial class AddPropertyAndReservationIdFilters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PropertyId",
+                table: "Trace",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReservationId",
+                table: "Trace",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PropertyId",
+                table: "Trace");
+
+            migrationBuilder.DropColumn(
+                name: "ReservationId",
+                table: "Trace");
+        }
+    }
+}
