32782db32158e321c9b55904515333fa839c5570
Peter <peter.grman@gmail.com>
2019-12-06 13:17:20 +0100

    add some initial logging features

---
+++ src/Traces.Core/LayoutRenderers/TenantIdLayoutRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text;
+using NLog;
+using NLog.LayoutRenderers;
+using NLog.Web.LayoutRenderers;
+using Traces.Common.Constants;
+
+namespace Traces.Core.LayoutRenderers
+{
+    [LayoutRenderer("tenant-id")]
+    public class TenantIdLayoutRenderer : AspNetLayoutRendererBase
+    {
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var context = HttpContextAccessor.HttpContext;
+
+            if (context == null)
+            {
+                return;
+            }
+
+            string accountCode = context.User.Claims.FirstOrDefault(x => x.Type == ApaleoClaims.AccountCode)?.Value;
+
+            builder.Append(accountCode);
+        }
+    }
+}
