37c5de0dd5a115b3d8b8767b9150f9e939af8a80
Randy Collado <randyro91@gmail.com>
2020-01-08 10:12:06 +0100

    Construct redirect url from config

---
+++ src/Traces.Common/ApaleoConfig.cs
@@ -0,0 +1,9 @@
+namespace Traces.Common
+{
+    public class ApaleoConfig
+    {
+        public string ClientId { get; set; }
+
+        public IntegrationConfig IntegrationConfig { get; set; }
+    }
+}
